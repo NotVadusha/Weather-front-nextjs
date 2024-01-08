cddffb99084cb969d805d0c7e1bf31649f809713
NotVadusha <vadimkbondarchuk@gmail.com>
2024-01-08 12:58:52 +0200

    feat: Add current weather component

---
+++ src/components/CurrentWeather.component.tsx
@@ -0,0 +1,70 @@
+import React, { useEffect, useState } from "react";
+import { Typography, Box } from "@mui/material";
+import Image from "next/image";
+import { WeatherResponse } from "../types/WeatherResponse";
+import { weatherService } from "../services/weather.service";
+
+interface CurrentWeatherProps {}
+
+export default function CurrentWeather() {
+  const [data, setData] = useState<WeatherResponse | null>(null);
+  useEffect(() => {
+    const weatherPromise = weatherService.getCurrentViaIp();
+    weatherPromise.then((res) => setData(res as WeatherResponse));
+  }, []);
+
+  return (
+    <Box
+      maxWidth={500}
+      p={2}
+      bgcolor={"white"}
+      display={"flex"}
+      flexDirection={"column"}
+      gap={2}
+    >
+      {data && (
+        <Box>
+          <Box>
+            <Typography variant="h6" color="initial">
+              Current weather
+            </Typography>
+            <Typography variant="body1" color="initial">
+              {new Date().toLocaleString("en-US", {
+                hour: "numeric",
+                minute: "numeric",
+                hour12: true,
+              })}
+            </Typography>
+          </Box>
+
+          <Box width={"fit-content"} mx={"auto"}>
+            <Image
+              src={"https:" + data.current.condition.icon}
+              width={100}
+              height={100}
+              alt="1"
+            />
+            <Box display={"inline-block"}>
+              <Typography variant="h2" color="initial">
+                {data.current.temp_c}°C
+              </Typography>
+              <Typography variant="body1" color="initial">
+                RealFeel {data.current.feelslike_c}°C
+              </Typography>
+            </Box>
+          </Box>
+          <Box display={"flex"} justifyContent={"space-between"}>
+            <Typography variant="body1" color="initial" width={"fit-content"}>
+              {data.current.condition.text}
+            </Typography>
+            <a href="">
+              <Typography variant="body1" color="initial" width={"fit-content"}>
+                MORE DETAILS
+              </Typography>
+            </a>
+          </Box>
+        </Box>
+      )}
+    </Box>
+  );
+}
