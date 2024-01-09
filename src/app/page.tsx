15108474391cb6c9b3a3bef9f266141d544b9003
NotVadusha <vadimkbondarchuk@gmail.com>
2024-01-09 16:31:52 +0200

    fix: Add additional info to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
-"use client";
 import { Box } from "@mui/material";
 import CurrentWeather from "../components/CurrentWeather.component";
+import AdditionalInfo from "../components/AdditionalInfo.component";
 
 const HomePage = () => {
   return (
     <main>
       <Box>
         <CurrentWeather />
+        <AdditionalInfo />
       </Box>
     </main>
   );
