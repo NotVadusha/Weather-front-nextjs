d34dc9248ad2c115b7a14ec5331d5cbc338d971f
NotVadusha <vadimkbondarchuk@gmail.com>
2024-01-19 19:50:41 +0200

    ref: Delete theme registry

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,70 @@
 "use client";
-import { Box } from "@mui/material";
+import { useState, useEffect } from "react";
+import { Box, Container, ThemeProvider, alpha } from "@mui/material";
 import CurrentWeather from "../components/CurrentWeather.component";
 import AdditionalInfo from "../components/AdditionalInfo.component";
 import TemperatureCharts from "../components/TemperatureCharts.component";
-import { useEffect, useState } from "react";
 import { weatherService } from "../services/weather.service";
-import { CurrentWeatherResponse } from "../types/CurrentWeatherResponse";
 import { ForecastResponse } from "../types/ForecastResponse";
+import { getMainBackground } from "../utils/backgroundUri";
+import { getTheme } from "../utils/materialTheme";
 
 const HomePage = () => {
   const [forecast, setForecast] = useState<ForecastResponse | null>(null);
+  const [date, setDate] = useState<Date>(new Date());
 
   useEffect(() => {
     const forecastPromise = weatherService.getForecastViaIp();
-    forecastPromise.then((res) => setForecast(res as ForecastResponse));
-  }, []);
+    forecastPromise.then((res) => {
+      setForecast(res as ForecastResponse);
+    });
+    setTimeout(() => {
+      setDate(new Date());
+    }, 60000 - date.getSeconds() * 1000);
+  }, [date]);
 
-  console.log(forecast);
+  const theme = getTheme(forecast ? !Boolean(forecast.current.is_day) : false);
 
   return (
     <main>
-      <Box>
-        {forecast && (
-          <>
-            <CurrentWeather currentWeather={forecast.current} />
-            <AdditionalInfo currentWeather={forecast.current} />
-          </>
-        )}
-        {forecast && forecast.forecast.forecastday[0] && (
-          <TemperatureCharts hours={forecast.forecast.forecastday[0].hour} />
-        )}
-      </Box>
+      <Box
+        sx={{
+          position: "relative",
+          "::before": {
+            content: '""',
+            position: "fixed",
+            left: 0,
+            right: 0,
+            zIndex: -1,
+            backgroundImage: `url(${getMainBackground(
+              Boolean(forecast?.current.is_day),
+              forecast ? forecast.current.temp_c : 0
+            )})`,
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",
+            backgroundPositionY: "bottom",
+            height: "100vh",
+            filter: "blur(15px)",
+          },
+        }}
+      ></Box>
+      <Container sx={{ paddingTop: "5rem" }} maxWidth={"md"}>
+        <ThemeProvider theme={theme}>
+          <Box p={5}>
+            {forecast && (
+              <>
+                <CurrentWeather date={date} forecast={forecast} />
+                <AdditionalInfo currentWeather={forecast.current} />
+              </>
+            )}
+            {forecast && forecast.forecast.forecastday[0] && (
+              <TemperatureCharts
+                hours={forecast.forecast.forecastday[0].hour}
+              />
+            )}
+          </Box>
+        </ThemeProvider>
+      </Container>
     </main>
   );
 };
