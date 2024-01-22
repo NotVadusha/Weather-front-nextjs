4552f2cfd7559d7436e04f325289f5963bab9548
NotVadusha <vadimkbondarchuk@gmail.com>
2024-01-22 16:48:01 +0200

    feat: Main color changes with temperature

--- src/utils/materialTheme.ts
+++ src/utils/materialTheme.ts
@@ -1,5 +1,6 @@
 import { ThemeOptions, createTheme } from "@mui/material";
 import { Poppins } from "next/font/google";
+import { TEMP_BREAKPOINTS } from "./backgroundUri";
 
 const poppinsFont = Poppins({
   weight: ["400", "500", "600", "700"],
@@ -7,25 +8,51 @@ const poppinsFont = Poppins({
   subsets: ["latin"],
 });
 
-export const getTheme = (preferDark: boolean) => {
+export const getTheme = (preferDark: boolean, temperature: number) => {
+  const mainColor =
+    temperature < TEMP_BREAKPOINTS.warm
+      ? temperature < TEMP_BREAKPOINTS.normal
+        ? "#3f51b5"
+        : "#66bb6a"
+      : "#FF5733";
+
   const themeOptions: ThemeOptions = {
     palette: {
       mode: preferDark ? "dark" : "light",
       primary: {
-        main: "#3f51b5",
+        main: mainColor,
       },
       secondary: {
         main: "#f50057",
+        contrastText: "rgba(255, 255, 255, 0.8)",
       },
     },
     typography: {
       fontSize: 16,
-      fontFamily: `${poppinsFont.style.fontFamily} sans-serif`,
+      fontFamily: `${poppinsFont.style.fontFamily}, sans-serif`,
       fontWeightLight: 400,
       fontWeightRegular: 600,
       fontWeightMedium: 700,
       fontWeightBold: 800,
       htmlFontSize: 18,
+    },
+    components: {
+      MuiTypography: {
+        defaultProps: {
+          variantMapping: {
+            h1: "h2",
+            h2: "h2",
+            h3: "h2",
+            h4: "h2",
+            h5: "h2",
+            h6: "h2",
+            subtitle1: "h2",
+            subtitle2: "h2",
+            body1: "span",
+            body2: "span",
+          },
+        },
+      },
     },
   };
 
